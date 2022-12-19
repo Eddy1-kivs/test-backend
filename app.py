5785e8b97ac27b85542217b10a41d4806757399a
edwinkivuva <kivseddy@gmail.com>
2022-12-19 21:37:16 +0300

    update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 import sqlite3
 import os
-# import psycopg2
 from dotenv import load_dotenv
 from models.billingHistory import billing
 from models.download import download_file
