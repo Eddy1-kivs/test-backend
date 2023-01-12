cdc9cdb616c3d2c03ec1eb9e7e379887218399e6
edwinkivuva <kivseddy@gmail.com>
2023-01-12 12:58:54 +0300

    update

--- auth/register.py
+++ auth/register.py
@@ -1,36 +1,34 @@
 import json
 import sqlite3
 import datetime
-
+import bcrypt
 from flask import Flask, request, jsonify, session, Blueprint
 
 get_started = Blueprint('get_started', __name__)
 
-
 def get_db():
     conn = sqlite3.connect('config/TestLoad.sqlite')
     return conn
-
 
 @get_started.route("/register", methods=["POST"])
 def signup():
     errors = {}
     required_fields = ['first_name', 'last_name', 'phone_number', 'username', 'email', 'password', 'location']
     for field in required_fields:
-        if not request.form.get(field):
+        if not request.get_json().get(field):
             errors[field] = 'This field is required'
 
     if errors:
         return jsonify(errors), 400
 
-    first_name = request.form.get('first_name')
-    last_name = request.form.get('last_name')
-    phone_number = request.form.get('phone_number')
-    username = request.form.get('username')
-    email = request.form.get('email')
-    password = request.form.get('password')
-    location = request.form.get('location')
-    img = request.form.get('img', '')
+    first_name = request.get_json().get('first_name')
+    last_name = request.get_json().get('last_name')
+    phone_number = request.get_json().get('phone_number')
+    username = request.get_json().get('username')
+    email = request.get_json().get('email')
+    password = request.get_json().get('password')
+    location = request.get_json().get('location')
+    img = request.get_json().get('img', '')
     created_at = datetime.datetime.utcnow().isoformat()
     updated_at = created_at
 
@@ -43,12 +41,15 @@ def signup():
     if user:
         return jsonify({'error': 'Username or email already in use'}), 400
 
+    # Hash the password before storing it
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    hashed_password = hashed_password.decode('utf-8')
+
     try:
         cursor.execute('INSERT INTO users (first_name, last_name, phone_number, username, email,'
                        ' password, location, img, created_at, updated_at)'
                        ' VALUES (?,?,?,?,?,?,?,?,?,?)',
-                       (first_name, last_name, phone_number, username, email, password, location, img, created_at,
-                        updated_at))
+                       (first_name, last_name, phone_number, username, email, hashed_password, location, img, created_at,updated_at))
         conn.commit()
         return {'success': 'User has been registered'}
     except sqlite3.Error as e:
