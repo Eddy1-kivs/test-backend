5251fc69e920fb2b9659f4bf3479bf1bc287a21e
edwinkivuva <kivseddy@gmail.com>
2023-01-14 12:16:02 +0300

    update

--- auth/login.py
+++ auth/login.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify, session, Blueprint, redirect
-import sqlite3
-from flask_session import Session
+from flask import Flask, request, jsonify, Blueprint
+from flask_jwt_extended import JWTManager, create_access_token
+import bcrypt
+import bcrypt
 
 sign_in = Blueprint('sign_in', __name__)
 
@@ -9,32 +10,33 @@ def get_db():
     conn = sqlite3.connect('config/TestLoad.sqlite')
     return conn
 
-
-@sign_in.route("/login", methods=["GET", 'POST'])
+@sign_in.route("/login", methods=["POST"])
 def login():
     errors = {}
-    requiredFields = ['username', 'password']
-
-    for field in requiredFields:
+    required_fields = ['username', 'password']
+    for field in required_fields:
         if not request.get_json().get(field):
             errors[field] = 'This field is required'
 
     if errors:
         return jsonify(errors), 400
 
-    defaultValue = ''
-    username = request.get_json().get('username', defaultValue)
-    password = request.get_json().get('password', defaultValue)
+    username = request.get_json().get('username')
+    password = request.get_json().get('password')
 
     conn = get_db()
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
+    cursor.execute('SELECT * FROM users WHERE username=?', (username,))
     user = cursor.fetchone()
     if user is None:
         errors['username'] = 'Invalid username or password'
         return jsonify(errors), 400
     else:
-        # log the user in and redirect to the home page
-        session['user_id'] = user[0]
-        return redirect('/home')
+        # Compare password
+        if not bcrypt.checkpw(password.encode('utf-8'), user[3].encode('utf-8')):
+            errors['password'] = 'Invalid username or password'
+            return jsonify(errors), 400
 
+    # log the user in by creating a JWT
+    access_token = create_access_token(identity=user[0])
+    return jsonify(access_token=access_token), 200
