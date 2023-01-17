7bbd8063e2f7173823f91bcd058e2219263a5685
edwinkivuva <kivseddy@gmail.com>
2023-01-17 22:25:07 +0300

    update

--- auth/login.py
+++ auth/login.py
@@ -60,8 +60,13 @@ def login():
     if user:
         # Compare password
         if bcrypt.checkpw(password.encode('utf-8'), user.password):
+            login = user
             access_token = create_access_token(identity=user.username)
-            return jsonify(access_token=access_token), 200
+            login = {
+                'id': login.id,
+                'username': login.username,
+            }
+            return jsonify(access_token=access_token, login=login), 200
         else:
             errors['password'] = 'Invalid password'
             return jsonify(errors), 400
