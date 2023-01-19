3eaa5f8f9104d5ab78d29fbfd8ef472040b446db
edwinkivuva <kivseddy@gmail.com>
2023-01-19 16:05:02 +0300

    update

--- views/logout.py
+++ views/logout.py
@@ -1,11 +1,12 @@
-from flask import request, jsonify, Blueprint
+from flask import request, jsonify, Blueprint, Response
 from flask_jwt_extended import jwt_required, unset_jwt_cookies
 
 logout = Blueprint('logout', __name__)
 
 
 @logout.route('/logout', methods=['POST'])
-@jwt_required
+@jwt_required()
 def logout_user():
+    response = Response()
     unset_jwt_cookies(request, response)
     return jsonify({'success': True})
