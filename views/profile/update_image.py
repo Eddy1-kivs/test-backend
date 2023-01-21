a783b3e25d02dfd1edcc7d6131752d5361e13342
edwinkivuva <kivseddy@gmail.com>
2023-01-21 16:04:00 +0300

    update

--- views/profile/update_image.py
+++ views/profile/update_image.py
@@ -51,7 +51,7 @@ def save_image(image):
     try:
         file_ext = os.path.splitext(image.filename)[1]
         file_name = f"{str(uuid.uuid4())}{file_ext}"
-        file_path = os.path.join("/images/", file_name)
+        file_path = os.path.join("images/", file_name)
         image.save(file_path)
     except:
         return None
